ad0210fbd7d867b0058b62c114c18f1af8ba7a0b
Jayanthpodila12 <jayanthpodila@gmail.com>
2025-02-25 16:05:16 +0530

    create terraform script

---
+++ Terraform/var.tf
@@ -0,0 +1,11 @@
+# Access Key
+variable "aws_access_key" {
+ type       = string
+ description= "Enter AWS Key"
+}
+
+# Secret Key
+variable "aws_secret_key" {
+ type       = string
+ description= "Enter AWS Key"
+}
